27752154b11b4fe7da624aa11f6687510b298c59
Max <maxvhanamane@gmail.com>
2023-01-07 00:01:05 +0530

    fixed a bug (showattendance page)

--- pages/api/addattendance.js
+++ pages/api/addattendance.js
@@ -10,8 +10,37 @@ export default async function handler(req, res) {
         // Check If todays attendance is taken or not. If it is the first one then we will add all other students attendance of that class
         // as NT (not taken). so that when we try to see a table using showattendace page at that time everything would look consistent.(if you  take an attendance of only one student and try to see a table then it doesn't look good to avoid that I am adding NT.)
         let todaysAttendanceStarted = await AttendanceRecord.find({ className: req.body.className, "date": { "$gte": startDate, "$lt": endDate } }) 
+
+        let allStudents = await Student.find({ className: req.body.className })
+
+        if(allStudents.length!=todaysAttendanceStarted.length){
+            // const set2 = new Set(todaysAttendanceStarted.map(obj => obj.name));
+
+            // // filter array1 to keep only objects whose name is not in the set
+            // const filteredArray = allStudents.filter(obj => !set2.has(obj.name));
+
+let attendance = new AttendanceRecord({
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone,
+            className: req.body.className,
+            division: req.body.division,
+            rollNumber: req.body.rollNumber,
+            attendance: req.body.attendance,
+            date: new Date(req.body.date),
+            time: req.body.time,
+            genRegNumber: req.body.genRegNumber,
+            caste: req.body.caste,
+            subCaste: req.body.subCaste,
+            DOB: req.body.DOB
+        })
+        await attendance.save()
+        return res.status(200).send({ success: "true" })
+
+        }
+
+
         if (todaysAttendanceStarted.length <= 0) {
-            let allStudents = await Student.find({ className: req.body.className })
             let newArray = allStudents.map((data) => {
                 let obj = data.toObject();
                 delete obj._id;
--- pages/showattendance.js
+++ pages/showattendance.js
@@ -14,21 +14,35 @@ export default function Attendancerecord({ allStudents, classNameValue }) {
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
     const [showAttendace, setShowAttendance] = useState(false)
-
     let dates = data.map((item) => {
         return parseInt(item.date.split("T")[0].split("-")[2])
     })
     let uniqueDates = [...new Set(dates.sort())];
 
+    let impData = studentDetails.sort((d, e) => { return d.rollNumber - e.rollNumber }).map((item, index) => {
 
+        let b = data?.filter(x => { return x.name === item.name }).sort(function (a, b) {
+            return new Date(a.date) - new Date(b.date);
+        })
+        if(b.length===0){
+            b.push({name:item.name})
+        }
+        if (b.length != uniqueDates.length) {
+            let newArray = uniqueDates.slice(0, uniqueDates.length - b.length);
+            newArray.forEach(() => {
+                b.unshift("Max")
+            })
 
-    
+        }
+        return b
+    })
+
 
     const getData = async () => {
-      
+
         let sDate = new Date(startDate).toISOString().substring(0, 10) + "T00:00:00Z"
         let eDate = new Date(endDate).toISOString().substring(0, 10) + "T23:59:59Z"
-        let data = { sDate, eDate,className,division }
+        let data = { sDate, eDate, className, division }
         const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/getdatewisedata`, {
             method: 'POST', // or 'PUT'
             headers: {
@@ -51,85 +65,85 @@ export default function Attendancerecord({ allStudents, classNameValue }) {
     return (
         <>
             <div className='w-full flex items-center justify-center flex-col md:flex-row fixed top-[3.25rem] z-10 bg-gray-50  border-b border-gray-800 pb-6 pt-4'>
-              <div className="flex gap-1">
-                <div className='flex flex-col md:flex-row gap-2 md:mx-2'>
+                <div className="flex gap-1">
+                    <div className='flex flex-col md:flex-row gap-2 md:mx-2'>
 
-                    <p className='my-auto mx-2 md:mx-auto'>Start Date:</p>
-                    <div>
-                        <DatePicker dateFormat="dd/MM/yyyy" className='bg-gray-100 text-center w-32 md:w-40 border-2 rounded border-gray-700' selected={startDate} onChange={(date) => setStartDate(date)} />
+                        <p className='my-auto mx-2 md:mx-auto'>Start Date:</p>
+                        <div>
+                            <DatePicker dateFormat="dd/MM/yyyy" className='bg-gray-100 text-center w-32 md:w-40 border-2 rounded border-gray-700' selected={startDate} onChange={(date) => setStartDate(date)} />
+                        </div>
+                    </div>
+
+                    <div className='flex flex-col md:flex-row gap-2'>
+                        <p className='my-auto mx-2 md:mx-auto'>End Date:</p>
+                        <div>
+                            <DatePicker dateFormat="dd/MM/yyyy" className='bg-gray-100  text-center w-32 md:w-40 border-2 rounded border-gray-700' selected={endDate} onChange={(date) => setEndDate(date)} />
+                        </div>
                     </div>
                 </div>
-
-                <div className='flex flex-col md:flex-row gap-2'>
-                    <p className='my-auto mx-2 md:mx-auto'>End Date:</p>
-                    <div>
-                        <DatePicker dateFormat="dd/MM/yyyy" className='bg-gray-100  text-center w-32 md:w-40 border-2 rounded border-gray-700' selected={endDate} onChange={(date) => setEndDate(date)} />
-                    </div>
-                </div>
-            </div>
                 <div className='flex mt-2 md:mt-auto'>
                     <button className='m-2  text-center bg-violet-900 text-white rounded p-0.5 px-1' onClick={getData}>Get attendance</button>
                     <button className='m-2  text-center bg-violet-900 text-white rounded p-0.5 px-1' onClick={getPdf}>Download pdf</button>
                 </div>
-              
+
             </div>
-           <div>
+            <div>
 
-           <h1 className='fixed top-36 w-full text-center font-bold  text-gray-800 lg:text-3xl text-xl mb-1 pb-4 '>{`Attendance of grade ${classNameValue}  students`} </h1>
+                <h1 className='fixed top-36 w-full text-center font-bold  text-gray-800 lg:text-3xl text-xl mb-1 pb-4 '>{`Attendance of grade ${classNameValue}  students`} </h1>
 
-           </div>
+            </div>
             {showAttendace ? <div>
                 {data.length > 0 ? <div className=" fixed top-52 h-[calc(100vh-13rem)]  overflow-auto  w-full  lg:px-12 mb-20" >
                     <div className="pb-16">
-                    <table className=" w-full overflow-auto text-sm text-left text-gray-500 " id="table_pdf" >
-                        
-                        <thead className="sticky top-0 text-xs  w-screen text-gray-700 uppercase  bg-gray-50  ">
-                            <tr >
-                                <th scope="col" className="py-3 px-2 text-center">
-                                   Sr.No
-                                </th>
-                                <th scope="col" className="py-3 px-2 text-center">
-                                    GR.No
-                                </th>
-                                <th scope="col" className="py-3 px-2 text-center">
-                                    DOB
-                                </th>
-                                <th scope="col" className="py-3 px-2 text-center">
-                                    Caste
-                                </th>
-                                <th scope="col" className="py-3 px-2 text-center">
-                                    Subcaste
-                                </th>
-                                <th scope="col" className="py-3 px-2 text-center">
-                                    R.No
-                                </th>
-                                <th scope="col" className="py-3 px-2 text-center">
-                                    Name
-                                </th>
+                        <table className=" w-full overflow-auto text-sm text-left text-gray-500 " id="table_pdf" >
 
-                                {/* <th scope="col" className="py-3 px-2 text-center">
+                            <thead className="sticky top-0 text-xs  w-screen text-gray-700 uppercase  bg-gray-50  ">
+                                <tr >
+                                    <th scope="col" className="py-3 px-2 text-center">
+                                        Sr.No
+                                    </th>
+                                    <th scope="col" className="py-3 px-2 text-center">
+                                        GR.No
+                                    </th>
+                                    <th scope="col" className="py-3 px-2 text-center">
+                                        DOB
+                                    </th>
+                                    <th scope="col" className="py-3 px-2 text-center">
+                                        Caste
+                                    </th>
+                                    <th scope="col" className="py-3 px-2 text-center">
+                                        Subcaste
+                                    </th>
+                                    <th scope="col" className="py-3 px-2 text-center">
+                                        R.No
+                                    </th>
+                                    <th scope="col" className="py-3 px-2 text-center">
+                                        Name
+                                    </th>
+
+                                    {/* <th scope="col" className="py-3 px-2 text-center">
                     P/A
                 </th> 
                 <th scope="col" className="py-3 px-2 text-center">
                     Att. Date
                 </th> */}
 
-                                {uniqueDates.map((item) => {
-                                    return <th key={item} scope="col" className="py-3 px-2 text-center"> {item} </th>
-                                })}
+                                    {uniqueDates.map((item) => {
+                                        return <th key={item} scope="col" className="py-3 px-2 text-center"> {item} </th>
+                                    })}
 
-                                <th scope="col" className="py-3 px-2 text-center">
-                                   Present days
-                                </th>
-                                <th scope="col" className="py-3 px-2 text-center">
-                                    Absent days
-                                </th>
+                                    <th scope="col" className="py-3 px-2 text-center">
+                                        Present days
+                                    </th>
+                                    <th scope="col" className="py-3 px-2 text-center">
+                                        Absent days
+                                    </th>
 
-                            </tr>
-                        </thead>
-                        <tbody className="font-semibold ">
-                            {/* data.sort((a,b)=>{return new Date(a.date) - new Date(b.date) }) */}
-                            {/* { data.sort((d,e)=>{return d.rollNumber-e.rollNumber }).map((item,index)=>{
+                                </tr>
+                            </thead>
+                            <tbody className="font-semibold ">
+                                {/* data.sort((a,b)=>{return new Date(a.date) - new Date(b.date) }) */}
+                                {/* { data.sort((d,e)=>{return d.rollNumber-e.rollNumber }).map((item,index)=>{
                 return <tr key={index} className="bg-white border-border-collapse   dark:bg-gray-800 dark:border-gray-700">
            
                  <td className="py-4 px-2 text-center">
@@ -169,66 +183,68 @@ export default function Attendancerecord({ allStudents, classNameValue }) {
 
 
 
-                            {studentDetails.sort((d, e) => { return d.rollNumber - e.rollNumber }).map((item, index) => {
-                                return <tr key={index} className="bg-white border-border-collapse   ">
+                                {studentDetails.sort((d, e) => { return d.rollNumber - e.rollNumber }).map((item, index) => {
+                                    return <tr key={index} className="bg-white border-border-collapse   ">
 
-                                    <td className="py-4 px-2 text-center">
-                                        {index + 1}
-                                    </td>
-                                    <td className="py-4 px-2 text-center">
-                                        {item.genRegNumber}
-                                    </td>
-                                    <td className="py-4 px-2 text-center">
-                                        {new Date(item.DOB).toLocaleDateString('en-GB')}
-                                    </td>
-                                    <td className="py-4 px-2 text-center">
-                                        {item.caste}
-                                    </td>
-                                    <td className="py-4 px-2 text-center">
-                                        {item.subCaste}
-                                    </td>
-                                    <td className="py-4 px-2 text-center">
-                                        {item.rollNumber}
-                                    </td>
-                                    <td className="py-4 px-2 text-center">
-                                        {item.name}
-                                    </td>
-                                    {/* // Turn your strings into dates, and then subtract them
+                                        <td className="py-4 px-2 text-center">
+                                            {index + 1}
+                                        </td>
+                                        <td className="py-4 px-2 text-center">
+                                            {item.genRegNumber}
+                                        </td>
+                                        <td className="py-4 px-2 text-center">
+                                            {new Date(item.DOB).toLocaleDateString('en-GB')}
+                                        </td>
+                                        <td className="py-4 px-2 text-center">
+                                            {item.caste}
+                                        </td>
+                                        <td className="py-4 px-2 text-center">
+                                            {item.subCaste}
+                                        </td>
+                                        <td className="py-4 px-2 text-center">
+                                            {item.rollNumber}
+                                        </td>
+                                        <td className="py-4 px-2 text-center">
+                                            {item.name}
+                                        </td>
+                                        {/* // Turn your strings into dates, and then subtract them
   // to get a value that is either negative, positive, or zero. */}
-                                    {/* {     console.log(  data.filter(x => { return x.name === item.name }).sort(function(a,b){
+                                        {/* {     console.log(  data.filter(x => { return x.name === item.name }).sort(function(a,b){
  
   return new Date(a.date) - new Date(b.date);
 }))} */}
 
-                                    {/* {console.log(data.filter(x => { return x.name === item.name }))} */}
+                                        {/* {console.log(data.filter(x => { return x.name === item.name }))} */}
 
-                                    {data.filter(x => { return x.name === item.name }).sort(function (a, b) {
-                                        return new Date(a.date) - new Date(b.date);
-                                    }).map((i, index) => {
-                                        return <td key={index} className={`py-4 ${i.attendance==="NT"?"text-red-600":null} font-semibold px-2 text-center`}>
-                                            {i.attendance}
+
+
+                                        {impData?.find(array => {
+                                            return array?.find(obj => obj.name === item.name)
+                                        }).map((i, index) => {
+                                            return <td key={index} className={`py-4 ${i.attendance === "NT" ? "text-red-600" : null} font-semibold px-2 text-center`}>
+                                                {i.attendance}
+                                            </td>
+                                        })}
+
+                                        <td className="py-4 px-2 text-center">
+                                            {
+                                                data.filter(x => { return x.name === item.name }).filter(item => item.attendance === 'P').length
+                                            }
                                         </td>
-                                    })}
 
-                                    <td className="py-4 px-2 text-center">
-                                        {
-                                            data.filter(x => { return x.name === item.name }).filter(item => item.attendance === 'P').length
-                                        }
-                                    </td>
-
-                                    <td className="py-4 px-2 text-center">
-                                        {
-                                            data.filter(x => { return x.name === item.name }).filter(item => item.attendance === 'A').length
-                                        }
-                                    </td>
+                                        <td className="py-4 px-2 text-center">
+                                            {
+                                                data.filter(x => { return x.name === item.name }).filter(item => item.attendance === 'A').length
+                                            }
+                                        </td>
 
 
-                                </tr>
+                                    </tr>
 
-                            })}
+                                })}
 
-                        </tbody>
-                    </table>
+                            </tbody>
+                        </table>
                     </div>
                 </div> : <div className="flex justify-center items-center font-semibold fixed w-full my-52 py-10 px-4 text-red-500">No attendance records found! </div>}
 
